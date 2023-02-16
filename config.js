06e4863bf2036e1a09c391efccd335e38b02a96f
pranavi-m <108517788+pranavi-m@users.noreply.github.com>
2023-02-16 14:02:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDo-Z1VI-6DAwf_p8Rs9Hdp8u6GVpZ9KwY",
+  authDomain: "e-library-app-86d9d.firebaseapp.com",
+  projectId: "e-library-app-86d9d",
+  storageBucket: "e-library-app-86d9d.appspot.com",
+  messagingSenderId: "538727668146",
+  appId: "1:538727668146:web:d440f9d178f3ac423e6cab"
+};
+
+if(!firebase.apps.length)
+{
+firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase.firestore();
